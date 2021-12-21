b27b28c04b5120cfe75badf3ddd740c7134ccafc
Alberto Morgante Medina <alknopfler@users.noreply.github.com>
2021-12-21 21:46:02 +0100

    add first scheleton for cli ztp

---
+++ cmd/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+
+	"github.com/spf13/cobra"
+
+	command "github.com/alknopfler/cli-ztp-deployment/pkg/cmd"
+)
+
+func main() {
+
+	if len(os.Getenv("GOMAXPROCS")) == 0 {
+		runtime.GOMAXPROCS(runtime.NumCPU())
+	}
+
+	command := newCommand()
+	if err := command.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+}
+
+func newCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "ztpcli",
+		Short: "Ztp is a command line to deploy ztp openshift clusters",
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Help()
+			os.Exit(1)
+		},
+	}
+
+	cmd.AddCommand(command.NewVerifyPreflights())
+
+	return cmd
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/alknopfler/cli-ztp-deployment
+
+go 1.16
+
+require (
+	github.com/spf13/cobra v1.3.0
+	k8s.io/cli-runtime v0.23.1
+)
