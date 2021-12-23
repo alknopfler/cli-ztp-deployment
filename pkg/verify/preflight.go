e5b221b9978795541c170d91b49684450df33d65
Alberto Morgante <alknopfler@gmail.com>
2021-12-23 14:24:10 +0100

    add preflight goroutines

--- pkg/verify/preflight.go
+++ pkg/verify/preflight.go
@@ -5,24 +5,48 @@ import (
 	"github.com/alknopfler/cli-ztp-deployment/config"
 	"github.com/alknopfler/cli-ztp-deployment/pkg/auth"
 	"github.com/alknopfler/cli-ztp-deployment/pkg/resources"
+	"k8s.io/client-go/kubernetes"
 	"log"
+	"sync"
 )
+
+var wg sync.WaitGroup
 
 func RunPreflights() error {
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+	client := auth.Set(config.Ztp.Config.KubeconfigHUB)
 
-	c := auth.Set(config.Ztp.Config.KubeconfigHUB)
+	wg.Add(2)
+	go verifyNodes(*client, ctx)
+	go verifyPVS(*client, ctx)
+	wg.Wait()
+	return nil
+}
 
-	pvs, err := resources.GetPVS(c, ctx)
+func verifyPVS(clientset kubernetes.Clientset, ctx context.Context) {
+	defer wg.Done()
+	pvs, err := resources.GetPVS(&clientset, ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	if len(pvs.Items) < 3 {
-		log.Fatal("Error PV insufficients...Exiting", err)
+		log.Fatal("Error PV insufficients...Exiting")
 	}
-	log.Println("Pvs validated")
-	return nil
+	log.Println(">>>> Pvs validated")
+}
+
+func verifyNodes(clientset kubernetes.Clientset, ctx context.Context) {
+	defer wg.Done()
+	nodes, err := resources.GetNodes(&clientset, ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(nodes.Items) < 3 {
+		log.Fatal("Errnodesn insufficients...Exiting")
+	}
+	log.Println(">>>> Nodes validated")
 }
