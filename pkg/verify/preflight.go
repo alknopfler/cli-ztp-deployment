ab825d4812295c39e3e0ce0224c5ddb59a6b3998
Alberto Morgante Medina <alknopfler@users.noreply.github.com>
2021-12-23 17:44:43 +0100

    add clusteroperators try

--- pkg/verify/preflight.go
+++ pkg/verify/preflight.go
@@ -2,9 +2,11 @@ package verify
 
 import (
 	"context"
+	"fmt"
 	"github.com/alknopfler/cli-ztp-deployment/config"
 	"github.com/alknopfler/cli-ztp-deployment/pkg/auth"
 	"github.com/alknopfler/cli-ztp-deployment/pkg/resources"
+	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
 	"log"
 	"sync"
@@ -17,10 +19,12 @@ func RunPreflights() error {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	client := auth.Set(config.Ztp.Config.KubeconfigHUB)
+	dynamicClient := auth.SetWithDynamic(config.Ztp.Config.KubeconfigHUB)
 
-	wg.Add(2)
+	wg.Add(3)
 	go verifyNodes(*client, ctx)
 	go verifyPVS(*client, ctx)
+	go verifyClusterOperators(dynamicClient, ctx)
 	wg.Wait()
 	return nil
 }
@@ -49,4 +53,15 @@ func verifyNodes(clientset kubernetes.Clientset, ctx context.Context) {
 		log.Fatal("[ERROR] nodes insufficient...Exiting")
 	}
 	log.Println(">>>> Nodes validated")
+}
+
+func verifyClusterOperators(client dynamic.Interface, ctx context.Context) {
+	defer wg.Done()
+	co, err := resources.GetResourcesDynamically(client, ctx, "config.openshift.io", "v1", "clusteroperators", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(len(co))
+	log.Println(">>>> co validated")
 }
