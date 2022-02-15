03a9c8627874443cfbb71c731576a7b56f0fd46b
alknopfler <amorgant@redhat.com>
2022-02-15 20:17:16 +0100

    fix typo with resource name in unstructure query
    Signed-off-by: Alberto Morgante Medina <alknopfler@users.noreply.github.com>

--- pkg/registry/verify.go
+++ pkg/registry/verify.go
@@ -202,7 +202,7 @@ func (r *Registry) verifyMCP(ctx context.Context, client dynamic.Interface) erro
 }
 
 func (r *Registry) verifyMachineConfig(ctx context.Context, client dynamic.Interface) error {
-	mcp, err := resources.NewGenericGet(ctx, client, "machineconfiguration.openshift.io", "v1", "machineconfig", "", "update-localregistry-ca-certs", "").GetResourceDynamically()
+	mcp, err := resources.NewGenericGet(ctx, client, "machineconfiguration.openshift.io", "v1", "machineconfigs", "", "update-localregistry-ca-certs", "").GetResourceDynamically()
 	if err != nil {
 		log.Printf(color.InRed("[ERROR] Error getting MachineConfig info: %e"), err.Error())
 		return err
